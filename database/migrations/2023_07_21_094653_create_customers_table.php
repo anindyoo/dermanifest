63b1dd3fb9c4cef7414dc99b68e73bc3e1b482ab
anindyoo <anindyo0216@gmail.com>
2023-07-22 00:19:47 +0700

    refactor: drop username column

--- database/migrations/2023_07_21_094653_create_customers_table.php
+++ database/migrations/2023_07_21_094653_create_customers_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name_customer');
-            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('phone');
