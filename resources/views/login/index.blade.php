b62623e7b50aabaf7d0e065dedcafa059cb17c5b
anindyoo <anindyo0216@gmail.com>
2023-07-23 00:46:51 +0700

    feat: implement google auth button partial

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -45,11 +45,7 @@
 
           <p class="text-center p-or">-OR-</p>
 
-          <div>
-            <a class="btn btn-outline-dark btn-login" href="" role="button" style="text-transform:none; background-color: white; width: 100%">
-              <img width="20px" style="width: 20px !important; margin-bottom:3px; margin-right:5px;" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" /> Log In with Google
-            </a>
-          </div>
+          @include('partials/google-button', ['type' => 'Log In'])
         </div>
 
         <div class="text-signup">
