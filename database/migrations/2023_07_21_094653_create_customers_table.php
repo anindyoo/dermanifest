82e9902d60130133266b66834ce0f36204bfda57
anindyoo <anindyo0216@gmail.com>
2023-07-22 18:42:11 +0700

    feat: add email verify timestamp column

--- database/migrations/2023_07_21_094653_create_customers_table.php
+++ database/migrations/2023_07_21_094653_create_customers_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('phone');
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('date_created')->nullable();
             $table->timestamps();
         });
