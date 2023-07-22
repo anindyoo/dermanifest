648bf1fe74778bbdfdcad1b8f1a8641e934ed7f4
anindyoo <anindyo0216@gmail.com>
2023-07-23 00:48:20 +0700

    feat: add controller for google oauth 2.0 feature

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Admin;
+use App\Models\Customer;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle() {
+        return Socialite::driver('google')->redirect(); 
+    }
+
+    public function googleCallbackHandler() {
+        $userGoogle = Socialite::driver('google')->user();
+
+        $findAdmin = Admin::where('email', $userGoogle->getEmail())->first();
+        $findUser = Customer::where('email', $userGoogle->getEmail())->first();
+        // $findUser = [2];
+        $current_time = Carbon::now();
+
+        if ($findUser) {
+            if ($findUser['email_verified_at'] == '') {
+                Customer::where('email', $userGoogle->getEmail())->update(['email_verified_at' => $current_time]);
+            }
+            Auth::guard('web')->login($findUser);
+            return redirect()->intended('/')->with('success', $findUser['email_verified_at']);
+        } elseif ($findAdmin) {
+            Auth::guard('admin')->login($findAdmin);
+            return redirect()->intended('admin')->with('success', 'Login with email:'. $findAdmin['email'] .' is successful!');            
+        } else {
+            $random_password = Hash::make(Str::random(10));
+            $newUser = Customer::create([
+                'name_customer' => $userGoogle->getName(),
+                'email' => $userGoogle->getEmail(),
+                'google_id' => $userGoogle->getId(),
+                'password' => $random_password,
+                'phone' => "",
+                "email_verified_at" => $current_time,
+            ]);
+
+            Auth::login($newUser);
+            return redirect()->intended('/');
+        }
+    }
+}
