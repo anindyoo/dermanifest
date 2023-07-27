8319deac4806374a633fe70ad1c6ddb4b23a2447
anindyoo <anindyo0216@gmail.com>
2023-07-27 20:34:16 +0700

    feat: add category crud methods

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,19 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index() {
+        $categoriesData = Category::all();
+
         return view('admin.categories.index', [
-            "title" => "Categories"
+            'title' => 'Categories',
+            'categories_data' => $categoriesData,
         ]);
     }
 
     public function create() {
         return view('admin.categories.create', [
-            "title" => "Add Category"
+            'title' => 'Add Category'
         ]);
+    }
+
+    public function edit($id) {
+        $categoryData = Category::findOrFail($id);
+
+        return view('admin.categories.edit', [
+            'title' => 'Add Category',
+            'category_data' => $categoryData,
+        ]);
+    }
+
+    public function store(Request $request) {
+        $validatedData = $request->validate([
+            'name_category' => 'required|max:255',
+            'description_category' => 'required',
+        ]);
+        Category::create($validatedData);
+
+        return redirect('admin/categories')->with('success', 'New Category: <strong>' . $request->name_category . '</strong> has been added.');
+    }
+
+    public function update(Request $request, Category $category) {
+        $validatedData = $request->validate([
+            'name_category' => 'required|max:255',
+            'description_category' => 'required',
+        ]);        
+        Category::where('id', $category->id)->update($validatedData);
+
+        return redirect('admin/categories')->with('success', 'Category has been updated.');
+    }
+
+    public function destroy(Category $category) {
+        Category::destroy($category->id);
+
+        return redirect('admin/categories')->with('success', 'Category: <strong>' . $category->name_category . '</strong> has been deleted.');
     }
 }
