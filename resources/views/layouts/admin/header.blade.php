69920689c0fbabeabe2853d09bc685524af4fc02
anindyoo <anindyo0216@gmail.com>
2023-07-27 15:40:50 +0700

    feat: add header layout for admin pages

---
+++ resources/views/layouts/admin/header.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    {{-- Bootstrap 5 --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    {{-- Boxicon --}}
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    {{-- Fa Icon --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    {{-- Jquery --}}
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    {{-- Local CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/admin/style.css') }}">
+    @yield('csrf_token')
+    <title>{{ $title }} | Admin Dermanifest</title>
+  </head>
+  <body>
+    @include('partials.admin.sidebar')
+    @yield('content')
+    <!-- Swiper JS -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <!-- Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Bootstrap 5 -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <!-- Select2 -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <!-- Local JS -->
+    <script src="{{ asset('js/main.js') }}"></script>
+    @yield('js_code')
+    <script>
+      // Toggle Menu
+      function toggleMenu() {
+        let toggle = document.querySelector('.toggle');
+        toggle.classList.toggle('active');
+    
+        let navi = document.querySelector('.navi');
+        navi.classList.toggle('active');
+    
+        let main = document.querySelector('.main');
+        main.classList.toggle('active');
+      }
+    </script>
+  </body>
+<html>
