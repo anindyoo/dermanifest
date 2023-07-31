0e5fa0d92e0c36a4a25cf468473094203d29aac5
anindyoo <anindyo0216@gmail.com>
2023-07-31 16:56:32 +0700

    feat: add controller for admin product pictures management page

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Picture;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class PictureController extends Controller
 {
-    //
+    public function show($product_id) {
+        $productData = (new Product)->getProductById($product_id);
+        $productPicsturesData = Picture::all();
+
+        return view('admin.products.pictures', [
+            'title' => 'Product Pictures Management',
+            'product_data' => $productData,
+            'product_pictures_data' => $productPicsturesData,
+        ]);
+    }
 }
