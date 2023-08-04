cf5bb803045ed76c8b2501b5822c5a3555d11117
anindyoo <anindyo0216@gmail.com>
2023-08-04 19:34:53 +0700

    add: add controller for home

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\TopProduct;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $topProductsData = TopProduct::all();
+        $productsData = [];
+
+        foreach ($topProductsData as $key => $top_product) {
+            if ($top_product->product_id != null){
+                $productData = (new Product)->getProductById($top_product->product_id);
+                $productsData[$key] = $productData;
+                $productsData[$key]['category_name'] = (new Category)->getCategoryById($productData->category_id)->name_category;
+            } else {
+                unset($topProductsData[$key]);
+            }
+        }
+
+        // dd($productsData);
+        return view('home', [
+            'title' => 'Home',
+            'products_data' => $productsData,
+        ]);
+    }
+}
