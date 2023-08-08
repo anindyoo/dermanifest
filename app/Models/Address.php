65a0de489d966cf54e72b27b9dac1f390d4bd66a
anindyoo <anindyo0216@gmail.com>
2023-08-08 12:08:02 +0700

    feat: add function to get addresses by customer id

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -10,4 +10,12 @@ class Address extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function orders() {
+        return $this->belongsToMany(Order::class);
+    }
+
+    public function getAddressesByCustomerId($id) {
+        return $this::where('customer_id', $id)->get();
+    }
 }
