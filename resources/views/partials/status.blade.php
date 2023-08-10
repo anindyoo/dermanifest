b4770797839ee9bc7a520eb5686be4149a36a4c2
anindyoo <anindyo0216@gmail.com>
2023-08-10 18:37:48 +0700

    feat: add status partials

---
+++ resources/views/partials/status.blade.php
@@ -0,0 +1,5 @@
+@if ($status == 'unpaid')
+  <span class="status-badge danger-status fw-bold">{{ ucfirst($status) }}</span>  
+@elseif (3 ==4)
+    ''
+@endif
---
+++ resources/views/partials/status_text.blade.php
@@ -0,0 +1,5 @@
+@if ($status == 'unpaid')
+  <span class="text-danger fw-bold">{{ ucfirst($status) }}</span>  
+@elseif (3 ==4)
+    ''
+@endif
