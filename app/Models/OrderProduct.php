268de1f903c57fa9217da2183f5592352b8de645
anindyoo <anindyo0216@gmail.com>
2023-08-10 18:39:29 +0700

    feat: add function to get order product by order id

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -10,4 +10,8 @@ class OrderProduct extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function getOrderProductsByOrderId($order_id) {
+        return $this::where('order_id', $order_id)->get();
+    }
 }
