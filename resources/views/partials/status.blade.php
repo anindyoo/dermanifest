1a0aa831af7e2daa0fcbd56122929e3a3becbef5
anindyoo <anindyo0216@gmail.com>
2023-08-16 13:36:12 +0700

    feat: add styles

--- resources/views/partials/status.blade.php
+++ resources/views/partials/status.blade.php
@@ -1,3 +1,16 @@
+<style>
+  .status-badge {
+    padding: 8px 6px;
+  }
+  .danger-status {
+    background-color: #dc3545;
+    color: #ffffff;
+  }
+  .success-status {
+      background-color: #198754;
+      color: #ffffff;
+  }
+</style>
 @if ($status == 'unpaid')
   <span class="status-badge danger-status fw-bold">{{ ucfirst($status) }}</span>  
 @elseif ($status == 'paid')
