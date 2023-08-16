5248297e16d28b5dbdfe3b84d061cb46f665ebbf
anindyoo <anindyo0216@gmail.com>
2023-08-16 23:47:42 +0700

    feat: implement order filter

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -12,37 +12,39 @@
       ])
     @endif
   </div>
-  <div class="table-wrapper" style="overflow-x: auto;">
-    <table class="table table-bordered table-striped table-hover">
-      <thead>
-        <tr>
-          <th>No.</th>
-          <th>Order Id</th>
-          <th>Recipient</th>
-          <th>Grand Total</th>
-          <th>Status</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($orders_data as $order)
-          <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $order->id }}</td>
-            <td>{{ $order->recipient }}</td>
-            <td>Rp{{ number_format($order->grand_total, 0, ', ', '.') }},-</td>
-            <td>@include('partials.status', ['status' => $order->status])</td><td>
-              @include('partials.status_button', [
-                'status' => $order->status,
-                'order_id' => $order->id,
-                'role' => 'admin'
-              ])
-            </td>
-          </tr>
-        @endforeach
-      </tbody>
-    </table>
+  <label for="select-status" class="form-label">Filter Order</label>
+  <select id="filter-order" class="form-select mb-3">
+    <option value="all-table">All Order</option>
+    <option value="unpaid-table">Unpaid</option>
+    <option value="paid-table">Paid</option>
+    <option value="delivering-table">Delivering</option>
+    <option value="completed-table">Completed</option>
+  </select>
+  <div id="all-table" class="table-wrapper">
+    @include('partials.order_table', ['orders' => $orders_data, 'status' => 'all', 'role' => 'admin'])
+  </div>
+  <div id="unpaid-table" class="table-wrapper d-none">
+    @include('partials.order_table', ['orders' => $orders_data, 'status' => 'unpaid', 'role' => 'admin'])
+  </div>
+  <div id="paid-table" class="table-wrapper d-none">
+    @include('partials.order_table', ['orders' => $orders_data, 'status' => 'paid', 'role' => 'admin'])
+  </div>
+  <div id="delivering-table" class="table-wrapper d-none">
+    @include('partials.order_table', ['orders' => $orders_data, 'status' => 'delivering', 'role' => 'admin'])
+  </div>
+  <div id="completed-table" class="table-wrapper d-none">
+    @include('partials.order_table', ['orders' => $orders_data, 'status' => 'completed', 'role' => 'admin'])
   </div>
 </div>
+@endsection
+@section('js_code')
+<script>
+$(document).ready(function() {
+  $('#filter-order').change(function() {
+    $('.table-wrapper').addClass('d-none');
+    $('#' + this.value).removeClass('d-none');
+  });
 
+});
+</script>
 @endsection
