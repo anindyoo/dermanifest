89b3424e4a2642ae6a2fefe5a97bd050d221bb06
anindyoo <anindyo0216@gmail.com>
2023-08-16 20:22:29 +0700

    feat: add status text for delivering & completed order

--- resources/views/partials/status_text.blade.php
+++ resources/views/partials/status_text.blade.php
@@ -1,5 +1,9 @@
 @if ($status == 'unpaid')
   <span class="text-danger fw-bold">{{ ucfirst($status) }}</span>  
 @elseif ($status == 'paid')
-  <span class="text-success fw-bold">{{ ucfirst($status) }}</span>  
+  <span class="text-success fw-bold">{{ ucfirst($status) }}</span>
+@elseif ($status == 'delivering')
+  <span class="text-primary fw-bold">{{ ucfirst($status) }}</span>
+@elseif ($status == 'completed')
+  <span class="text-primary-native fw-bold">{{ ucfirst($status) }}</span>
 @endif
