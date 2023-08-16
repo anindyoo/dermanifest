df48a7acac735b491d529fbf4bfd71b05e9108bf
anindyoo <anindyo0216@gmail.com>
2023-08-16 20:27:41 +0700

    feat: add methods for updating & deleting order

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\OrderAddress;
+use App\Models\OrderPayment;
 use App\Models\OrderProduct;
 use Illuminate\Http\Request;
 
@@ -22,12 +23,47 @@ class AdminOrderController extends Controller
         $orderData = Order::find($id);
         $orderProductsData = (new OrderProduct)->getOrderProductsByOrderId($id);
         $orderAddressData = (new OrderAddress)->getOrderAddressByOrderId($id);
+        $orderPaymentData = (new OrderPayment)->getOrderPaymentByOrderId($id);    
 
         return view('admin.orders.show', [
             'title' => 'Order Detail',
             'order_data' => $orderData,
             'order_products' => $orderProductsData,
             'order_address' => $orderAddressData,
+            'order_payment' => $orderPaymentData,
         ]);
+    }
+
+    public function edit($id) {
+        $orderData = Order::find($id);
+        $orderPaymentData = (new OrderPayment)->getOrderPaymentByOrderId($id);    
+        
+        return view('admin.orders.edit', [
+            'title' => 'Confirm Order Payment',
+            'order_data' => $orderData,
+            'order_payment' => $orderPaymentData,
+        ]);
+    }
+
+    public function update(Request $request) {
+        $validatedData = $request->validate([
+            'status' => 'required|max:20',
+            'delivery_code' => 'required|max:30',
+        ]);
+
+        Order::where('id', $request['order_id'])->update([
+            'status' => $validatedData['status'],
+            'delivery_code' => $validatedData['delivery_code']
+        ]);
+
+        return redirect('/admin/orders/' . $request['order_id'] .'/edit')->with('success', 'Order has been confirmed.');
+    }
+
+    public function destroy(Order $order) {
+        $orderProducts = OrderProduct::where('order_id', $order->id)->get();
+        $this->readdStock($orderProducts);
+        Order::destroy($order->id);
+
+        return redirect('/admin/orders')->with('success', '<strong> Order #' . $order->product_id . '</strong> has been canceled.');
     }
 }
