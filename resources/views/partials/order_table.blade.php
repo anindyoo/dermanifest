56d8ca1da9a684ae9ff6c903d13646a92432f7d1
anindyoo <anindyo0216@gmail.com>
2023-08-16 23:45:14 +0700

    feat: add partial for seperating order table

---
+++ resources/views/partials/order_table.blade.php
@@ -0,0 +1,53 @@
+<div class="table-responsive">
+  @if (isset($role) AND $role == 'admin')
+  <table class="table table-bordered table-striped table-hover">
+  @else    
+  <table class="table">
+  @endif
+    <thead class="table-light manrope-font">
+      <tr>
+        <th scope="col">No.</th>
+        <th scope="col">Order Id</th>
+        @if (isset($role) AND $role == 'admin') <th scope="col">Recipient</th> @endif
+        <th scope="col">Date</th>
+        <th scope="col">Grand Total</th>
+        <th scope="col">Status</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      @forelse ($orders as $order)
+      @if ($status == 'all' OR $status == $order->status)
+        <tr>
+          <th scope="row" class="manrope-font">{{ $loop->iteration }}.</th>
+          <td>{{ $order->id }}</td>
+          @if (isset($role) AND $role == 'admin') <td>{{ $order->recipient }}</td> @endif
+          <td>{{ $order->created_at }} WIB</td>
+          <td>Rp{{ number_format($order->grand_total, 0, ', ', '.') }},-</td>
+          <td>@include('partials.status', ['status' => $order->status])</td>
+          <td>
+            @if (isset($role) AND $role == 'admin')
+              @include('partials.status_button', [
+                'status' => $order->status,
+                'order_id' => $order->id,
+                'role' => 'admin',
+                ])                          
+            @else
+              @include('partials.status_button', [
+                'status' => $order->status,
+                'order_id' => $order->id,
+                ])                
+            @endif
+          </td>
+        </tr>                            
+      @endif
+      @empty
+      <tr>
+        <td colspan="100%" class="text-center">
+          <h5>You have no orders, yet.</h5>
+        </td>
+      </tr>
+      @endforelse
+    </tbody>
+  </table>
+</div>
