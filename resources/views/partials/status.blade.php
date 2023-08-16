3952777e4359ca290286f86e6728fcd4f179b2ed
anindyoo <anindyo0216@gmail.com>
2023-08-16 20:22:16 +0700

    feat: add status badge for delivering & completed order

--- resources/views/partials/status.blade.php
+++ resources/views/partials/status.blade.php
@@ -3,16 +3,28 @@
     padding: 8px 6px;
   }
   .danger-status {
-    background-color: #dc3545;
-    color: #ffffff;
+  background-color: #dc3545;
+  color: #ffffff;
   }
   .success-status {
-      background-color: #198754;
-      color: #ffffff;
+    background-color: #198754;
+    color: #ffffff;
+  }
+  .delivering-status {
+    background-color: #0d6efd;
+    color: #ffffff;
+  }
+  .delivering-completed {
+    background-color: #333d29;
+    color: #ffffff;
   }
 </style>
 @if ($status == 'unpaid')
   <span class="status-badge danger-status fw-bold">{{ ucfirst($status) }}</span>  
 @elseif ($status == 'paid')
   <span class="status-badge success-status fw-bold">{{ ucfirst($status) }}</span>  
+@elseif ($status == 'delivering')
+  <span class="status-badge delivering-status fw-bold">{{ ucfirst($status) }}</span>  
+@elseif ($status == 'completed')
+  <span class="status-badge delivering-completed fw-bold">{{ ucfirst($status) }}</span>  
 @endif
