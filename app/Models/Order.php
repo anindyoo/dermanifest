10d032437950e185e9ce3ac9fdfa55cc608bc397
anindyoo <anindyo0216@gmail.com>
2023-08-17 17:33:28 +0700

    feat: add method for getting order by customer id

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,4 +10,8 @@ class Order extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function getOrderByCustomerId($customer_id) {
+        return $this::where('customer_id', $customer_id)->get();
+    }
 }
