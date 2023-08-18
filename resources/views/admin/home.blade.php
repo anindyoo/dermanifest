e1399a8e8e4d625cae77f801a8d82efd5b68dd08
anindyoo <anindyo0216@gmail.com>
2023-08-18 09:13:46 +0700

    feat: implement sales data on admin home page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,28 +8,28 @@
     <div class="col-md my-card">
       <div class="card-icon"><h1><i class="fa-solid fa-users"></i></h1></div>
       <div class="card-desc">
-        <div class="card-value text-end"><h5>20</h5></div>
+        <div class="card-value text-end"><h5>{{ $customers_total }}</h5></div>
         <div class="card-value-desc text-end"><h3>Customers</h3></div>
       </div>
     </div>
     <div class="col-md my-card">
       <div class="card-icon"><h1><i class="fa-solid fa-cart-shopping"></i></h1></div>
       <div class="card-desc">
-        <div class="card-value text-end"><h5>14</h5></div>
+        <div class="card-value text-end"><h5>{{ $orders_total }}</h5></div>
         <div class="card-value-desc text-end"><h3>Overrall Orders</h3></div>
       </div>
     </div>
     <div class="col-md my-card">
       <div class="card-icon"><h1><i class="fa-solid fa-clock"></i></h1></div>
       <div class="card-desc">
-        <div class="card-value text-end"><h5>2</h5></div>
+        <div class="card-value text-end"><h5>{{ $paid_orders_total }}</h5></div>
         <div class="card-value-desc text-end"><h3>Waiting Orders</h3></div>
       </div>
     </div>
     <div class="col-md my-card">
       <div class="card-icon"><h1><i class="fa-solid fa-money-bill-trend-up"></i></h1></div>
       <div class="card-desc">
-        <div class="card-value text-end"><h5>Rp1.000.000,-</h5></div>
+        <div class="card-value text-end"><h5>Rp{{ number_format($gross_sales, 0, ', ', '.') }},-</h5></div>
         <div class="card-value-desc text-end"><h3>Gross Sales</h3></div>
       </div>
     </div>
