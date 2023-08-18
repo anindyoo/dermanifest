9d99169172a42147c4e0b55f58c57d14931b0b79
anindyoo <anindyo0216@gmail.com>
2023-08-18 20:51:46 +0700

    feat: add methods for adding faq

--- app/Http/Controllers/FAQController.php
+++ app/Http/Controllers/FAQController.php
@@ -15,4 +15,20 @@ class FAQController extends Controller
             'faqs_data' => $faqsData,
         ]);
     }
+
+    public function create() {
+        return view('admin.faqs.create', [
+            'title' => 'Add FAQ',
+        ]);
+    }
+
+    public function store(Request $request) {
+        $validatedData = $request->validate([
+            'question' => 'required|max:255',
+            'answer' => 'required',
+        ]);
+        Faq::create($validatedData);
+
+        return redirect('admin/faqs')->with('success', 'New FAQ has been added.');
+    }
 }
