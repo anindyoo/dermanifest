6c8cb824050499b7b0b03203c56a5475ae9cd133
anindyoo <anindyo0216@gmail.com>
2023-08-18 14:47:12 +0700

    feat: add methods for adding new admin

--- app/Http/Controllers/AdminManagementController.php
+++ app/Http/Controllers/AdminManagementController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 
 class AdminManagementController extends Controller
 {
@@ -14,5 +16,25 @@ class AdminManagementController extends Controller
             'title' => 'Admins Management',
             'admins_data' => $adminsData,
         ]);
+    }
+
+    public function create() {
+        return view('admin.admins_management.create', [
+            'title' => 'Add Admin',
+        ]);
+    }
+    
+    public function store(Request $request) {
+        $validatedData = $request->validate([
+            'name_admin' => 'required|max:255',
+            'email' => 'required|email:dns|unique:customers|unique:admins',
+            'password' => 'required|between:6,255|confirmed',
+        ]);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+        $admin = Admin::create($validatedData);
+
+        event(new Registered($admin));
+
+        return redirect('/admin/admins_management')->with('success', 'Admin: <strong>' . $request->name_admin . '</strong> has successfully been added.');
     }
 }
