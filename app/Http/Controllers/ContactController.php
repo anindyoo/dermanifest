9920f97d2227c2a5f33e33a913e239d3a00c11cb
anindyoo <anindyo0216@gmail.com>
2023-08-23 13:23:53 +0700

    feat: add method for submiting message

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,4 +18,19 @@ class ContactController extends Controller
         ]);
     }
 
+    public function store(Request $request) {
+        if (Auth::check()) {
+            $validatedData = $request->validate([
+                'email' => 'required|email:dns|max:255',
+                'phone' => 'required|numeric|digits_between:8,13',
+                'subject' => 'required|max:255',
+                'message' => 'required',
+            ]);
+            $validatedData['customer_id'] = Auth::user()->id;
+            Message::create($validatedData);
+            return back()->with('success', 'Message has successfully been sent! Thank you.');
+        }
+
+        return back()->with('danger', 'Please login first in order to submit a message.');
+    }
 }
