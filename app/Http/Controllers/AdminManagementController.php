0f3495df3173ff3c94514574752df091461eacb1
anindyoo <anindyo0216@gmail.com>
2023-08-28 00:06:43 +0700

    feat: implement logging & add method to show admin log activity detail page

--- app/Http/Controllers/AdminManagementController.php
+++ app/Http/Controllers/AdminManagementController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admin;
+use App\Models\LogActivity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
@@ -10,6 +11,7 @@ use Illuminate\Auth\Events\Registered;
 class AdminManagementController extends Controller
 {
     public function index() {
+        LogActivity::storeLogActivity('Membuka halaman Admins Management.', 'admin');
         $adminsData = Admin::all();
 
         return view('admin.admins_management.index', [
@@ -19,8 +21,21 @@ class AdminManagementController extends Controller
     }
 
     public function create() {
+        LogActivity::storeLogActivity('Membuka halaman Add Admin.', 'admin');
         return view('admin.admins_management.create', [
             'title' => 'Add Admin',
+        ]);
+    }
+
+    public function showAdminLog($admin_id) {
+        $logData = LogActivity::getLogActivityByAdminId($admin_id);
+        $adminData = Admin::find($admin_id);
+        LogActivity::storeLogActivity('Membuka halaman Admin Log Activity: ' . $adminData->name_admin . '.', 'admin');
+
+        return view('admin.admins_management.log_activity', [
+            'title' => 'Admin Log Activities Detail',
+            'admin_data' => $adminData,
+            'log_data' => $logData,
         ]);
     }
     
@@ -32,12 +47,14 @@ class AdminManagementController extends Controller
         ]);
         $validatedData['password'] = Hash::make($validatedData['password']);
         $admin = Admin::create($validatedData);
+        LogActivity::storeLogActivity('Membuat akun Admin baru: ' . $validatedData['name_admin'] . ' .', 'admin');
 
         return redirect('/admin/admins_management')->with('success', 'Admin: <strong>' . $request->name_admin . '</strong> has successfully been added.');
     }
 
     public function destroy(Request $request, Admin $admin) {        
         Admin::destroy($request->id);
+        LogActivity::storeLogActivity('Menghapus akun Admin: ' . $admin->name_admin . '.', 'admin');
 
         return redirect('/admin/admins_management')->with('success', 'Admin: <strong>' . $admin->name_admin . '</strong> has been deleted.');
     }
