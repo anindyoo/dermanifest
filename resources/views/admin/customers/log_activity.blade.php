faacfa054eb3ef0fa3f4c8c2b3185aef63305295
anindyoo <anindyo0216@gmail.com>
2023-08-28 00:07:42 +0700

    feat: add view for customer log activity detail page

---
+++ resources/views/admin/customers/log_activity.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.admin.header')
+
+@section('content')
+<div class="content">
+  <h2>Customer Log Activities Detail</h2>
+  <hr>
+  <div>
+    @if(session()->has('success'))
+      @include('partials/alert', [
+        'status' => 'success',
+        'message' => session('success'),
+      ])
+    @endif
+  </div>
+  <h5 class="fw-bold mb-2">Customer Detail</h5>
+  <table class="table table-bordered table-striped table-hover table-header-on-left">
+    <tbody>
+      <tr>
+        <th>Customer Id</th>
+        <td>{{ $customer_data->id }}</td>
+      </tr>
+      <tr>
+        <th>Name</th>
+        <td>{{ $customer_data->name_customer }}</td>
+      </tr>
+    </tbody>
+  </table>
+  <h5 class="fw-bold mb-2">Log Activities</h5>
+  <div class="table-wrapper table-responsive">
+    <table class="table table-bordered table-striped table-hover">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Log Id</th>
+          <th>Activity</th>
+          <th>URL</th>
+          <th>Method</th>
+          <th>Ip</th>
+          <th>User Agent</th>
+          <th>Date Created</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($log_data as $log)
+          <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $log->id }}</td>        
+            <td>{{ $log->activity }}</td>
+            <td>{{ $log->url }}</td>
+            <td>{{ $log->method }}</td>
+            <td>{{ $log->ip }}</td>
+            <td>{{ $log->agent }}</td>
+            <td>{{ $log->created_at }} WIB</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
+@endsection
