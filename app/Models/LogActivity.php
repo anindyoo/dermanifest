b0dc9ccf2437d6b79ea129aa50b6888dd9794416
anindyoo <anindyo0216@gmail.com>
2023-08-31 19:42:05 +0700

    feat: add addtional if

--- app/Models/LogActivity.php
+++ app/Models/LogActivity.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Facades\Auth;
-use PhpParser\Node\Stmt\ElseIf_;
 
 class LogActivity extends Model
 {
@@ -15,26 +14,28 @@ class LogActivity extends Model
     protected $guarded = ['id'];
 
     public static function storeLogActivity($activity, $role = null) {
-        if (self::orderBy('created_at', 'desc')->first()->role != 'disabled') {
-            $log = [
-                'activity' => $activity,
-                'role' => 'guest',
-                'url' => Request::fullUrl(),
-                'method' => Request::method(),
-                'ip' => Request::ip(),
-                'agent' => Request::header('user-agent'),                
-            ];
-            if (Auth::check()) {
-                if ($role == 'admin') {
-                    $log['user_id'] = Auth::user()->id;
-                    $log['role'] = 'admin';
+        if (self::all()->count() > 0) {
+            if (self::orderBy('created_at', 'desc')->first()->role != 'disabled') {
+                $log = [
+                    'activity' => $activity,
+                    'role' => 'guest',
+                    'url' => Request::fullUrl(),
+                    'method' => Request::method(),
+                    'ip' => Request::ip(),
+                    'agent' => Request::header('user-agent'),                
+                ];
+                if (Auth::check()) {
+                    if ($role == 'admin') {
+                        $log['user_id'] = Auth::user()->id;
+                        $log['role'] = 'admin';
+                    }
+                    if (Customer::where('email', Auth::user()->email)->exists()) {
+                        $log['user_id'] = Auth::user()->id;
+                        $log['role'] = 'customer';
+                    }
                 }
-                if (Customer::where('email', Auth::user()->email)->exists()) {
-                    $log['user_id'] = Auth::user()->id;
-                    $log['role'] = 'customer';
-                }
+                return self::create($log);
             }
-            return self::create($log);
         }
     }
 
