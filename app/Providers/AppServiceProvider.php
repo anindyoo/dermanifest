7b97db75cbbf9b69ae01e76f2b727060f4d1d140
anindyoo <anindyo0216@gmail.com>
2023-09-02 12:20:00 +0700

    feat: add default admin account

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Admin;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -23,6 +25,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {   
+        if (Admin::all()->count() == 0) {
+            Admin::create([
+                'name_admin' => 'Dermanifest Admin (Main)',
+                'email' => env('ADMIN_DEFAULT_EMAIL'),
+                'password' => Hash::make(env('ADMIN_DEFAULT_PASSWORD')),
+            ]);
+        }
+
         if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
