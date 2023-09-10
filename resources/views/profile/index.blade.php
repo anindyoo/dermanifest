25159cbe7d88e4d14a074d1045eb63fe44a2a280
anindyoo <anindyo0216@gmail.com>
2023-09-10 13:18:34 +0700

    fix: fix buttons view

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -186,8 +186,8 @@
         'modal_body' => '
         Are you sure to delete address: <strong>' . $addy->name_address . '</strong>?',
         'modal_footer' => '
-        <button type="submit" class="btn btn-outline-danger"><span class="fa-regular fa-trash-can me-1"></span>Delete Address</button>
-        <button type="button" class="btn btn-primary-native-regular" data-bs-dismiss="modal"><span class="fa-solid fa-pen-to-square me-1"></span>Cancel Delete</button>
+        <button type="button" class="btn btn-secondary-native-regular" data-bs-dismiss="modal"></span>Cancel Delete</button>
+        <button type="submit" class="btn btn-danger"><span class="fa-regular fa-trash-can me-1"></span>Delete Address</button>
         ',
       ])
     @endforeach      
