bb2c917f85abcd1bc290948eefebe0c532c85f3f
anindyoo <anindyo0216@gmail.com>
2024-01-04 18:07:06 +0700

    feat: update google logo icon path.

--- resources/views/partials/google-button.blade.php
+++ resources/views/partials/google-button.blade.php
@@ -1,6 +1,6 @@
 <div>
   <a class="btn btn-outline-dark btn-login" href="{{ route('google.login') }}" role="button" style="text-transform:none; background-color: white; width: 100%">
-    <img width="20px" style="width: 20px !important; margin-bottom:3px; margin-right:5px;" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+    <img width="20px" style="width: 20px !important; margin-bottom:3px; margin-right:5px;" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/640px-Google_%22G%22_logo.svg.png" />
     {{ $type }} with Google
   </a>
 </div>
